6b75af3135cfbf9ab04744f1e218440380b1b96f
Vivek Vardhan <75030674+vivekvardhan22@users.noreply.github.com>
2025-04-15 17:15:22 +0530

    Update settings.py

--- everythingvit/settings.py
+++ everythingvit/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4^@5kw27wm+0h1rzvba1=t@$xy8cr+axl232zi!ua91vhpuitq'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-key')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
