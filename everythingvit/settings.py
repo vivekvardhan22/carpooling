5bac147b4d27ddfec117d0fef22302446745bdd5
Vivek Vardhan <75030674+vivekvardhan22@users.noreply.github.com>
2025-04-15 17:29:43 +0530

    Update settings.py

--- everythingvit/settings.py
+++ everythingvit/settings.py
@@ -129,9 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'everythingvit/static')
-]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
